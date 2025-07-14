bbf7901f95f53a892aa248e3b758071676db5da2
Praveen D R <praveendr07@gmail.com>
2025-07-15 00:33:27 +0530

    update:made some changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import Calculator from "@/components/Calculator";
 
+
 export default function Home() {
   return (
-    <div className="text-red-600 bg-amber-300">
-      hi this is Calculator page
-
-    </div>
+    <main>
+      <Calculator/>
+    </main>
   );
 }
